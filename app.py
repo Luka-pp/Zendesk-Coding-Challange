6c838140e05e855314662351238263717971ffeb
luka_pp <luka_pp@hotmail.com>
2021-11-26 18:11:03 +0000

    Added rexuierements.txt

--- app.py
+++ app.py
@@ -20,23 +20,26 @@ zenpy_client = Zenpy(**creds)
 
 @app.route('/')
 def ticket_viewer():
-    tickets = zenpy_client.tickets()
+    ticket_generator = zenpy_client.tickets()
+    tickets = ticket_generator[0:25]
     return render_template('index.html', tickets=tickets)
 
 
-@app.route('/ticket_details/')
-def ticket_details():
-    return render_template('ticket_details.html')
+@app.route('/ticket_details/<ticket_id>')
+def ticket_details(ticket_id):
+    ticket = zenpy_client.tickets(id=ticket_id)
+    print(ticket)
+    return render_template('ticket_details.html', ticket=ticket)
 
 
 @app.errorhandler(404)
 def handle_404(exception):
-    return render_template("404.html", exception=exception)
+    return render_template("404_500.html", exception=exception)
 
 
 @app.errorhandler(500)
 def handle_500(exception):
-    return render_template("404.html", exception=exception)
+    return render_template("404_500.html", exception=exception)
 
 
 if __name__ == '__main__':
